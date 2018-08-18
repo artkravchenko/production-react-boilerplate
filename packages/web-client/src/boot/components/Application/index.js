1561e9b93b2d80b1e6e93791c1ca1d86e0e3fdd4
Artem Kravchenko <kravchenkofirst@gmail.com>
2018-08-18 23:54:24 +0700

    feat: add a demo of CSS assets serving and bundling support (#2)

--- packages/web-client/src/boot/components/Application/index.js
+++ packages/web-client/src/boot/components/Application/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+import Header from 'web-client/src/shared/widgets/Header';
+
 function Application() {
-  return <div>Hello world!</div>;
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Hello world!</div>
+    </React.Fragment>
+  );
 }
 
 export default hot(module)(Application);
---
+++ packages/web-client/src/shared/widgets/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './style.css';
+
+function Header() {
+  return <div className="header">This is the header</div>;
+}
+
+export default Header;
