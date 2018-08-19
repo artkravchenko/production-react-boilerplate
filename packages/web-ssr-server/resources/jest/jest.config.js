05d545070372f64cb7e3290708694a4823a4d4da
Artem Kravchenko <kravchenkofirst@gmail.com>
2018-08-19 18:59:45 +0700

    fix(test): shim .css and .scss import within jest environment (#2)

--- packages/web-ssr-server/resources/jest/jest.config.js
+++ packages/web-ssr-server/resources/jest/jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   collectCoverage: true,
   coverageDirectory: './resources/jest/coverage',
+  moduleNameMapper: {
+    '\\.(css|scss)$': '<rootDir>/../shared/__mocks__/style.js',
+  },
   rootDir: '../../',
   setupTestFrameworkScriptFile: './resources/jest/init.js',
   transform: {
