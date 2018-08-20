c4ad107e3bcdd6759a9d257e6e73653cf6676db6
Artem Kravchenko <kravchenkofirst@gmail.com>
2018-08-20 12:38:13 +0700

    refactor: use readlinkf() in the web-server's start script (#2)

--- packages/web-server/scripts/run.sh
+++ packages/web-server/scripts/run.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-script_path="$(cd "$(dirname "$0")" ; pwd -P)"
-nginx_resources_path="$(cd ${script_path}/../resources/nginx ; pwd -P)"
+# Keep in sync with $PROJECT/packages/shared/scripts/path/readlinkf.sh
+readlinkf() {
+  TARGET_FILE=$1
+
+  cd `dirname $TARGET_FILE`
+  TARGET_FILE=`basename $TARGET_FILE`
+
+  # Iterate down a (possible) chain of symlinks
+  while [ -L "$TARGET_FILE" ]
+  do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+  done
+
+  # Compute the canonicalized name by finding the physical path 
+  # for the directory we're in and appending the target file.
+  PHYS_DIR=`pwd -P`
+  RESULT=$PHYS_DIR/$TARGET_FILE
+  echo $RESULT
+}
+
+PROJECT=`readlinkf "$(dirname "$(readlinkf "${BASH_SOURCE[0]}")")/../../.."`
+
+script_path="$PROJECT/packages/web-server/scripts"
+nginx_resources_path="$PROJECT/packages/web-server/resources/nginx"
 
 for filename in $nginx_resources_path"/templates/*.template.conf"; do
   ${script_path}/subenv.sh "${filename}" \
